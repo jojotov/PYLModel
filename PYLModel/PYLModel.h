295800c1ef34a618910ec4a94260d4b8a4290c86
yulei pang <yuleipang@yuleideMacBook-Pro.local>
2019-01-29 08:35:32 +0800

    Initial Commit

---
+++ PYLModel/PYLModel.h
@@ -0,0 +1,19 @@
+//
+//  PYLModel.h
+//  PYLModel
+//
+//  Created by yulei pang on 2019/1/29.
+//  Copyright © 2019 pangyulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PYLModel.
+FOUNDATION_EXPORT double PYLModelVersionNumber;
+
+//! Project version string for PYLModel.
+FOUNDATION_EXPORT const unsigned char PYLModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PYLModel/PublicHeader.h>
+
+
---
+++ PYLModelTests/PYLModelTests.m
@@ -0,0 +1,37 @@
+//
+//  PYLModelTests.m
+//  PYLModelTests
+//
+//  Created by yulei pang on 2019/1/29.
+//  Copyright © 2019 pangyulei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PYLModelTests : XCTestCase
+
+@end
+
+@implementation PYLModelTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
